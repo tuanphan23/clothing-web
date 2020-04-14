72af46d4ffd95a467da9fa4e7cc9ad28681d98b9
Tuan Phan <tphan@seattleu.edu>
2020-04-14 02:03:32 -0700

    implemented firebase utils, included ability to store authenticated user into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,19 +1,44 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyBepL_0J7nNLTPRjrY6oY3CGPZdG_yMM_Q",
-    authDomain: "clothing-db-29a0b.firebaseapp.com",
-    databaseURL: "https://clothing-db-29a0b.firebaseio.com",
-    projectId: "clothing-db-29a0b",
-    storageBucket: "clothing-db-29a0b.appspot.com",
-    messagingSenderId: "860407452492",
-    appId: "1:860407452492:web:6327df5b22e138865d35e9",
-    measurementId: "G-EDJGNRQFT9"
-  };
+  apiKey: "AIzaSyBepL_0J7nNLTPRjrY6oY3CGPZdG_yMM_Q",
+  authDomain: "clothing-db-29a0b.firebaseapp.com",
+  databaseURL: "https://clothing-db-29a0b.firebaseio.com",
+  projectId: "clothing-db-29a0b",
+  storageBucket: "clothing-db-29a0b.appspot.com",
+  messagingSenderId: "860407452492",
+  appId: "1:860407452492:web:6327df5b22e138865d35e9",
+  measurementId: "G-EDJGNRQFT9",
+};
 
-firebase.initializeApp(config); 
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
